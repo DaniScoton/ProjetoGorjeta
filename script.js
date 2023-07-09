289e716b8050d70f6c667da8e07907e12dc37577
Dani Scoton <daniellyscoton@gmail.com>
2023-07-08 22:26:23 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("valor-calculado").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+function calcularJantar(){
+    var totalConta = document.getElementById("preco").value;
+    var taxaServico = document.getElementById("taxa-select").value;
+    var numeroPessoas = document.getElementById("pessoas").value;
+
+    if(totalConta === "" || taxaServico == 0 || isNaN(totalConta) || isNaN(numeroPessoas)){
+        alert("Por favor, coloque um valor válido!");
+        return;
+    }
+
+    if (numeroPessoas === "" || numeroPessoas <= 1){
+        numeroPessoas = 1;
+        document.getElementById("each").style.display = "none"
+    } else {
+        document.getElementById("each").style.display = "block"
+    }
+
+    var totalTaxaServico = (totalConta * taxaServico) / numeroPessoas;
+    var total = totalTaxaServico + (totalConta / numeroPessoas);
+
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+
+    document.getElementById("valor-calculado").style.display = "block";
+    document.getElementById("valor-por-cabeca").innerHTML = total;
+}
+
+document.getElementById("botao").onclick = function(){
+    calcularJantar();
+}
